1c5d6327aa80527f4c98886ac81a2d121c87d07b
alizade551 <merkuri.93@mail.ru>
2024-03-28 17:56:53 +0400

    LocalStrategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/domain/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from './hashing/hashing.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async validateLocal(email: string, password: string) {
+    const user = await this.userRepository.findOne({
+      select: {
+        id: true,
+        password: true,
+      },
+      where: { email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid email');
+    }
+    const isMatch = await this.hashingService.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    return { id: user.id };
+  }
+}
