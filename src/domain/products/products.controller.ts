a3da4eb0d2955ad7f17d05df8df686acdded48ac
alizade551 <merkuri.93@mail.ru>
2024-04-02 16:27:51 +0400

    Validating incoming files

--- src/domain/products/products.controller.ts
+++ src/domain/products/products.controller.ts
@@ -9,6 +9,9 @@ import {
   Query,
   UseInterceptors,
   UploadedFile,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  FileTypeValidator,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -48,7 +51,17 @@ export class ProductsController {
 
   @Post(':id/image')
   @UseInterceptors(FileInterceptor('file'))
-  uploadImage(@UploadedFile() file: Express.Multer.File) {
+  uploadImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 2 * 1024 * 1024 }),
+          new FileTypeValidator({ fileType: /png|jpeg/ }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
     return file;
   }
 }
